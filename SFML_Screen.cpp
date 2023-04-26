adeb69328331fd452ad6588a753368e75aa5e7ed
Vincent-RIVIERE <vincent.riviere@etu.sorbonne-universite.fr>
2023-04-26 16:46:06 +0200

    26/04/2023

--- SFML_Screen.cpp
+++ SFML_Screen.cpp
@@ -1,12 +1,25 @@
 #include <SFML/Graphics.hpp>
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode(1000, 700), "test");
     sf::CircleShape shape(100.f);
     sf::Color color = sf::Color::Red;
     shape.setFillColor(color);
 
     while (window.isOpen()) {
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+            shape.move(-1.f,0.f);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+            shape.move(1.f,0.f);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+            shape.move(0.f,-1.f);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+            shape.move(0.f,1.f);
+        }
 
         sf::Event event;
         while (window.pollEvent(event)) {
